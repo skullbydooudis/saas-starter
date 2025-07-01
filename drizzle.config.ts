142f093d2ae6afa4d586d6c506af600a41cfcd1c
skullbydooudis <fernan.kg34@gmail.com>
2025-07-01 16:58:00 -0300

    Fix Database Connection Issues

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -3,8 +3,8 @@ import type { Config } from 'drizzle-kit';
 export default {
   schema: './lib/db/schema.ts',
   out: './lib/db/migrations',
-  dialect: 'postgresql',
+  dialect: 'sqlite',
   dbCredentials: {
-    url: process.env.POSTGRES_URL!,
+    url: './dev.db',
   },
 } satisfies Config;
